8dc5e262aabb6e5d65ccf3428876d9e8ca24f4ee
Veronica-me <veronn.adams@gmail.com>
2022-04-24 23:07:12 +0200

    init 2

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* providers my app */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
